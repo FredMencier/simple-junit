e637bfaf29bf508d1a9604f0a087685713304bb3
Frederic Mencier <fmencier@yahoo.fr>
2024-02-11 11:17:12 +0100

    add dynamic tests

---
+++ SimpleJunit/src/test/java/MyMathTestParametrized.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+
+class MyMathTestParametrized {
+
+    private MyMath myMath;
+
+    @BeforeEach
+    public void setup() {
+        myMath = new MyMath();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        myMath = null;
+    }
+
+    @ParameterizedTest
+    @DisplayName("Test de la liste avec paramètres")
+    @ValueSource(ints = {0,1,2,3,4,5,6,7,8,9})
+    public void testGetListParam0(int index) {
+        Assertions.assertNotNull(myMath.getIntegerList().get(index));
+    }
+
+}
